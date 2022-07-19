35b08122864f11ed32e391a4fe47a30a7b00dcf2
Zach Carter <zacarter93@gmail.com>
2022-07-19 20:07:59 +0900

    created seeds file and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,29 @@
 require 'faker'
 
-puts 'Wiping database'
-Surplus.destroy_all
-Booking.destroy_all
-User.destroy_all
 puts 'Planting new seeds'
 
+zach = User.new(email: 'zach@rails.com', password: 'testing')
+zach.save
+
+p 'Welcome Zach!'
+
 10.times do
-  zach = User.new(email: 'zach@rails.com', password: 'testing')
-  zach.save
-  p zach.id
   Surplus.create!(
-    category: ['Japanese', 'Chinese', 'German', 'French', 'American'].sample,
+    category: rand(1..5),
     description: Faker::Food.description,
     location: ['Meguro, Tokyo', 'Kamata, Tokyo', 'Nagoya, Aichi', 'Sapporo, Hokkaido', 'Matsumoto, Nagano'].sample,
     user_id: zach.id,
     quantity: rand(1..10)
   )
   puts 'creating new surplus entry'
+end
+
+10.times do
+  Booking.create!(
+    date: rand(1..14),
+    status: %i[pending accepted rejected].sample,
+    user_id: zach.id,
+    surplus_id: Surplus.all.sample.id
+  )
+  puts 'creating new booking entry based on user and surplus'
 end
