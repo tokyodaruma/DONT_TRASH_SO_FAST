6e71d1dd2dce35fa8377004d8d748930230dc6f6
Jennifer Grohe <jennifer.grohe@gmail.com>
2022-07-19 20:49:51 +0900

    done

---
+++ app/controllers/surpluses_controller.rb
@@ -0,0 +1,26 @@
+class SurplusesController < ApplicationController
+
+  def index
+    @surplus = Surplus.all
+  end
+
+  # show
+
+  def new
+    @surplus = Surplus.new
+  end
+
+  def create
+    @surplus = Surplus.new(surplus_params)
+    if @surplus.save
+      redirect_to surplus_path(@surplus)
+    else
+      render :new
+    end
+  end
+  private
+
+  def surplus_params
+    params.require(:surplus).permit(:photo, :category, :description, :location, :quantity)
+  end
+end
