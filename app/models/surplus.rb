d2868a1d71e07d203b84e39369b7f1203bfa9d13
Jennifer Grohe <jennifer.grohe@gmail.com>
2022-07-21 21:08:06 +0900

    validation

--- app/models/surplus.rb
+++ app/models/surplus.rb
@@ -2,4 +2,6 @@ class Surplus < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
   validates :category, :description, :location, :quantity, presence: true
+  validates :category, numericality: { greater_than_or_equal_to: 1 }
+  enum category: %i[bakery restaurant individual ngo butcher]
 end
