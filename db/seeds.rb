f799f13ffbf0e729f0a80eaff5a00bd7674dbec6
Zach Carter <zacarter93@gmail.com>
2022-07-21 20:01:35 +0900

    integrated cloudinary with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,21 @@
 require 'faker'
+require 'open-uri'
 
-puts 'Planting new seeds'
-
-zach = User.new(email: 'zach@rails.com', password: 'testing')
-zach.save
-
-p 'Welcome Zach!'
+puts 'Destroy previous seeds.'
+Booking.destroy_all
+Surplus.destroy_all
+puts 'Planting new seeds.'
 
 10.times do
-  Surplus.create!(
+  file = URI.open('https://loremflickr.com/320/240/bread')
+  surplus = Surplus.create!(
     category: rand(1..5),
     description: Faker::Food.description,
     location: ['Meguro, Tokyo', 'Kamata, Tokyo', 'Nagoya, Aichi', 'Sapporo, Hokkaido', 'Matsumoto, Nagano'].sample,
-    user_id: zach.id,
+    user_id: 2,
     quantity: rand(1..10)
   )
+  surplus.photo.attach(io: file, filename: 'leftovers.png', content_type: 'image/png')
   puts 'creating new surplus entry'
 end
 
@@ -22,7 +23,7 @@ end
   Booking.create!(
     date: rand(1..14),
     status: %i[pending accepted rejected].sample,
-    user_id: zach.id,
+    user_id: 2,
     surplus_id: Surplus.all.sample.id
   )
   puts 'creating new booking entry based on user and surplus'
