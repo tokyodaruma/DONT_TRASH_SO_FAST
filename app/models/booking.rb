d9b387263bd7b8929b55d5b3a957e48ba05f2406
Zach Carter <zacarter93@gmail.com>
2022-07-21 21:34:26 +0900

    corrected validations for surplus category

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,6 @@ class Booking < ApplicationRecord
   belongs_to :surplus
 
   enum status: %i[pending accepted rejected]
+  validates :status, inclusion: { in: statuses.keys }
   validates :date, :status, presence: true
 end
--- app/models/surplus.rb
+++ app/models/surplus.rb
@@ -2,6 +2,7 @@ class Surplus < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
   validates :category, :description, :location, :quantity, presence: true
-  validates :category, numericality: { greater_than_or_equal_to: 1 }
+  validates :quantity, numericality: { greater_than_or_equal_to: 1 }
   enum category: %i[bakery restaurant individual ngo butcher]
+  validates :category, inclusion: { in: categories.keys }
 end
