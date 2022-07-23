f63722d52190fe483ab4ef766873eb62c779e71f
Jennifer Grohe <jennifer.grohe@gmail.com>
2022-07-23 10:49:20 +0900

    changed index layout a bit

--- app/models/surplus.rb
+++ app/models/surplus.rb
@@ -2,7 +2,7 @@ class Surplus < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
   validates :category, :description, :location, :quantity, presence: true
-  validates :quantity, numericality: { greater_than_or_equal_to: 1 }
+  validates :quantity, numericality: { only_integer: true }, inclusion: { in: 0..10, allow_nil: false }
   enum category: %i[bakery restaurant individual ngo butcher]
   validates :category, inclusion: { in: categories.keys }
 end
