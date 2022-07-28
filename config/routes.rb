e0bb2db1928c86f73ba696a1deed115bc2f0cbdc
antonio jurado <iberico.cortador@gmail.com>
2022-07-28 20:17:14 +0900

    accept-reject-routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :surpluses, only: %i[index show new create] do
-    resources :bookings, only: %i[create]
+    resources :bookings, only: %i[create update]
   end
   resources :bookings, only: %i[index]
 end
