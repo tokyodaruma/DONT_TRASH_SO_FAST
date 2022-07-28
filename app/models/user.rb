83e33955586208a0412083e1eedf65a9be437986
Satoka Sotome <satoka121@gmail.com>
2022-07-28 20:00:47 +0900

    added lister? method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :bookings
-  has_many :surpluses, through: :booking
+  has_many :surpluses, through: :bookings
+
+  def lister?
+    self.surpluses.any?
+  end
 end
