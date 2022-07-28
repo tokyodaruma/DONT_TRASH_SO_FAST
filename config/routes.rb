4f5a8b03c426fc788aff040ac3d0e76e8435a78d
Satoka Sotome <satoka121@gmail.com>
2022-07-28 21:43:15 +0900

    started  routes/controllers and policy

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :lister do
+    resources :bookings
+    # equivalent to => get ‘/lister/bookings’, to: ‘lister/bookings#index’
+    resources :surpluses
+  end
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :surpluses, only: %i[index show new create] do
-    resources :bookings, only: %i[create]
+    resources :bookings, only: %i[create update]
   end
   resources :bookings, only: %i[index]
 end
