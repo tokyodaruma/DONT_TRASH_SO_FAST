8e4007721ade5bfa15b1b3e3e107b244ee68319d
antonio jurado <iberico.cortador@gmail.com>
2022-07-30 15:11:56 +0900

    added new user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ Booking.destroy_all
 Surplus.destroy_all
 puts 'Planting new seeds.'
 
-zach = User.new(email: 'fake@fake.com', password: '1111111')
+zach = User.new(email: 'myfake@fake.com', password: '1111111', category:'individual')
 zach.save
 
 10.times do
